7b43a24b64e72d61c13e80d07fb4b6f0e9025251
Frontend_met <frontend.metapher@gmail.com>
2024-03-25 23:20:38 +0530

    chnages ts file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+    output: 'export',
+  };
+  
